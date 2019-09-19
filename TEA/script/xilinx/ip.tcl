d671a3a291812e2227c35c50176f2c7826dc77ae
AdahilMuniz <adahilmuniz@hotmail.com>
2019-09-18 23:47:25 -0300

    Complete IP packaging script

--- TEA/script/xilinx/ip.tcl
+++ TEA/script/xilinx/ip.tcl
@@ -1,28 +1,43 @@
 #!/usr/bin/tclsh
 
-set projdir [ file normalize "../../[ file dirname [ info script ] ]" ] 
+#@TODO: Invetigate the following warning:
+#           - WARNING: [IP_Flow 19-5101] Packaging a component with a SystemVerilog top file is not fully supported. Please refer to UG1118 'Creating and Packaging Custom IP'.
+
+set rootdir [ file normalize "../../[ file dirname [ info script ] ]" ]
+set projdir "$rootdir/VIVADO_PROJECT"
 
 # Xillinx Project Variables
 set projname TEA_VIVADO_PROJECT
-set vivado_projdir [ file normalize [ file dirname "$projdir/$projname" ] ] 
-set partnumber xc7a35tcpg236-1
+set partnumber "xc7a35tcpg236-1"
 
 # Design Variables
-set incdirs [list $projdir]
+set incdirs [list "$rootdir/verilog"]
+
+# Environment settings
+if ![file exists $projdir]  {file mkdir $projdir}
 
 # Create project
 create_project -force $projname $projdir -part $partnumber
-set_property board_part digilentinc.com:basys3:part0:1.1 [current_project]
+#set_property board_part digilentinc.com:basys3:part0:1.1 [current_project]
 
 # Add File
 set_property include_dirs $incdirs [current_fileset]; #Include Directories
 
-add_files -norecurse $filetop
-add_files -norecurse -scan_for_includes "$projdir/verilog/tea.svh"
-add_files -norecurse -scan_for_includes {"$projdir/verilog/tea.sv" "$projdir/verilog/tea_apb_wrapper.sv"}
+add_files -norecurse -scan_for_includes $rootdir/verilog/tea.svh
+add_files -norecurse -scan_for_includes $rootdir/verilog/tea.sv
+add_files -norecurse -scan_for_includes $rootdir/verilog/tea_apb_wrapper.sv
 
 #Setting top
 set_property top tea_apb_wrapper [current_fileset]
 
 #Compiler
 update_compile_order -fileset sources_1
+
+#Creating IP
+ipx::package_project -import_files { $rootdir/verilog/tea.svh $rootdir/verilog/tea.sv $rootdir/verilog/tea_apb_wrapper.sv } -root_dir $projdir -vendor SoinMicroelectronic.org -library soin_ip -taxonomy /SoinIP -generated_files
+set_property core_revision 2 [ipx::current_core]
+ipx::create_xgui_files [ipx::current_core]
+ipx::update_checksums [ipx::current_core]
+ipx::save_core [ipx::current_core]
+set_property  ip_repo_paths  $projdir [current_project]
+update_ip_catalog; #Is it necessary?
